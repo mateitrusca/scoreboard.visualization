15ce115c08a06d6218a7129ae48ffedfb7b7e0e9
Alex Morega <alex@grep.ro>
2012-11-22 19:36:42 +0200

    move Indicator class to App namespace

--- scoreboard/static/demo.js
+++ scoreboard/static/demo.js
@@ -1,26 +1,25 @@
 (function() {
 
 
-App.initialize = function() {
+App.Indicator = can.Control({
+    'init': function(element, options) {
+        $.getJSON(App.STATIC + '/indicators.json', function(data) {
+            element.html(can.view('indicator_ejs', new can.Observe(data)));
+            var value = can.route.attr()['indicator'];
+            element.find('select').val(value);
+        });
+    },
+    'select change': function(element, evt) {
+        var value = $(element).val();
+        can.route.attr({indicator: value});
+    }
+});
 
+
+App.initialize = function() {
     var view = can.view('chart_ejs');
     $('#the-chart').empty().append(view);
-
-    var Indicator = can.Control({
-        'init': function(element, options) {
-            $.getJSON(App.STATIC + '/indicators.json', function(data) {
-                element.html(can.view('indicator_ejs', new can.Observe(data)));
-                var value = can.route.attr()['indicator'];
-                element.find('select').val(value);
-            });
-        },
-        'select change': function(element, evt) {
-            var value = $(element).val();
-            can.route.attr({indicator: value});
-        }
-    });
-    var indicator_control = new Indicator('#the-indicator', {});
-
+    var indicator_control = new App.Indicator('#the-indicator', {});
 };
 
 
