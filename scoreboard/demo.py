a463a7401cea931724075a4c993a9537346f9f2c
Alex Morega <alex@grep.ro>
2012-11-23 15:02:31 +0200

    move templates to separate files

--- scoreboard/demo.py
+++ scoreboard/demo.py
@@ -1,11 +1,18 @@
 import simplejson as json
 from zope.interface import Interface
 import jinja2
+from path import path
+
+
+def get_templates():
+    templates_dir = path(__file__).abspath().parent / 'templates' / 'js'
+    return {t.namebase: t.text() for t in templates_dir.listdir()}
 
 
 jinja_env = jinja2.Environment(
     loader=jinja2.PackageLoader(__name__, 'templates'))
 jinja_env.globals['STATIC'] = '/++resource++scoreboard'
+jinja_env.globals['get_templates'] = get_templates
 jinja_env.filters['json'] = lambda v: jinja2.Markup(json.dumps(v))
 
 
