6f2bff85349bdc0893dc703827cad40a2bfd3ea7
Alex Morega <alex@grep.ro>
2012-11-23 12:01:12 +0200

    use backbone to render charts view

--- scoreboard/static/demo.js
+++ scoreboard/static/demo.js
@@ -18,16 +18,35 @@ App.Indicator = can.Control({
 
 App.Chart = can.Control({
     'route': function(data) {
-        this.element.html(can.view('chart_ejs', data));
+        App.filters.set(data);
     }
 });
 
 
+App.ChartView = Backbone.View.extend({
+
+    'initialize': function() {
+        this.model.on('change', this.render, this);
+        this.render();
+    },
+
+    'render': function() {
+        this.$el.html(can.view('chart_ejs', this.model.toJSON()));
+    }
+
+});
+
+
 App.initialize = function() {
+    App.filters = new Backbone.Model();
     var view = can.view('chart_ejs');
     $('#the-chart').empty().append(view);
     var indicator_control = new App.Indicator('#the-indicator', {});
-    var chart_control = new App.Chart('#the-chart', {});
+    var chart_control = new App.Chart();
+    new App.ChartView({
+        model: App.filters,
+        el: $('#the-chart')
+    });
 };
 
 
