e4911dfe9438c9e6f39644f70dfe003f5a3c1b6b
Alex Morega <alex@grep.ro>
2012-12-05 14:18:19 +0200

    display data loaded via ajax

--- scoreboard/static/demo.js
+++ scoreboard/static/demo.js
@@ -82,11 +82,20 @@ App.ChartView = Backbone.View.extend({
 App.MetadataView = Backbone.View.extend({
 
     initialize: function() {
+        this.model.on('change', this.render, this);
         this.render();
     },
 
     render: function() {
-        this.$el.html(App.render('metadata', {}));
+        this.$el.html("loading ...");
+        var args = App.make_filter_args(this.model);
+        var $el = this.$el;
+        if(args) {
+            _(args).extend({'method': 'get_indicator_meta'});
+            $.get(App.URL + '/data', args, function(data) {
+                $el.html(App.render('metadata', data[0]));
+            });
+        }
     }
 
 });
@@ -130,6 +139,7 @@ App.initialize = function() {
     });
 
     new App.MetadataView({
+        model: App.filters,
         el: $('#the-metadata')
     });
 
--- scoreboard/static/tests.js
+++ scoreboard/static/tests.js
@@ -109,3 +109,48 @@ describe('ChartView', function() {
     });
 
 });
+
+
+describe('MetadataView', function() {
+    "use strict";
+
+    var server;
+
+    beforeEach(function() {
+        server = sinon.fakeServer.create();
+    });
+
+    afterEach(function () {
+        server.restore();
+    });
+
+    it('should display data from server', function() {
+        var view = new App.MetadataView({
+            model: new Backbone.Model({'indicator': 'asdf', 'year': '2002'})
+        });
+
+        expect(server.requests.length).to.equal(1);
+        var url = server.requests[0].url;
+        expect(url_param(url, 'method')).to.equal(
+            'get_indicator_meta');
+        expect(url_param(url, 'indicator')).to.equal(
+            'http://data.lod2.eu/scoreboard/indicators/asdf');
+        expect(url_param(url, 'year')).to.equal(
+            'http://data.lod2.eu/scoreboard/year/2002');
+
+        var ajax_data = [{
+            'label': "The Label!",
+            'comment': "The Definition!",
+            'publisher': "The Source!"
+        }];
+        server.requests[0].respond(200, {'Content-Type': 'application/json'},
+                                   JSON.stringify(ajax_data));
+
+        expect(view.$el.find('h3').text()).to.equal("The Label!");
+        expect(view.$el.find('li:first').text()).to.contain.string(
+            "The Definition!");
+        expect(view.$el.find('li:last').text()).to.contain.string(
+            "The Source!");
+    });
+
+});
