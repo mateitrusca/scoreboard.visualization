16d46044d2aba671b6bc397028eff4593ef05c99
Alex Morega <alex@grep.ro>
2012-12-17 17:07:27 +0200

    split demo.js into common.js and scenario1.js

--- scoreboard/static/scenario1.js
+++ scoreboard/static/scenario1.js
@@ -114,27 +114,6 @@ App.Scenario1ChartView = Backbone.View.extend({
 });
 
 
-App.IndicatorMetadataView = Backbone.View.extend({
-
-    initialize: function() {
-        this.model.on('change', this.render, this);
-        this.render();
-    },
-
-    render: function() {
-        this.$el.html("loading ...");
-        var args = App.make_scenario1_filter_args(this.model);
-        var $el = this.$el;
-        if(args) {
-            _(args).extend({'method': 'get_indicator_meta'});
-            $.get(App.URL + '/data', args, function(data) {
-                $el.html(App.render('metadata', data[0]));
-            });
-        }
-    }
-
-});
-
 
 App.Scenario1Router = Backbone.Router.extend({
 
