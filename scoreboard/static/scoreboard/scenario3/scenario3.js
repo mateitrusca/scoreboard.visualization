ab54845ea73226c126ffb718c58c84a6f395a3a5
Alex Morega <alex@grep.ro>
2013-01-10 17:41:59 +0200

    select X axis

--- scoreboard/static/scoreboard/scenario3/scenario3.js
+++ scoreboard/static/scoreboard/scenario3/scenario3.js
@@ -2,6 +2,31 @@
 "use strict";
 
 
+App.Scenario3FiltersView = Backbone.View.extend({
+
+    events: {
+        'change select': 'update_filters'
+    },
+
+    initialize: function(options) {
+        this.filters_data = JSON.stringify(options['filters_data']);
+        this.render();
+    },
+
+    render: function() {
+        var data = JSON.parse(this.filters_data);
+        this.$el.html(App.render('scoreboard/scenario3/filters.html', data));
+    },
+
+    update_filters: function() {
+        this.model.set({
+            'indicator_x': this.$el.find('select[name=indicator_x]').val()
+        });
+    }
+
+});
+
+
 App.scenario3_initialize = function() {
     var box = $('#scenario-box');
     box.html(App.render('scoreboard/scenario3/scenario3.html'));
---
+++ scoreboard/static/scoreboard/scenario3/tests.js
@@ -0,0 +1,24 @@
+describe('Scenario3FiltersView', function() {
+    "use strict";
+
+    var choose = App.testing.choose_option;
+
+    beforeEach(function() {
+        this.filters_data = {'indicators': [{'uri': 'ind1'}, {'uri': 'ind2'}]};
+        this.model = new Backbone.Model();
+        this.view = new App.Scenario3FiltersView({
+            model: this.model,
+            filters_data: this.filters_data
+        });
+    });
+
+    describe('indicators selector', function() {
+
+        it('should select x axis', function() {
+            choose(this.view.$el.find('select[name=indicator_x]'), 'ind2');
+            expect(this.model.get('indicator_x')).to.equal('ind2');
+        });
+
+    });
+
+});
