2648a9ac42d4808f19b4756e7c76749b791ffbd5
Alex Morega <alex@grep.ro>
2013-01-10 17:28:32 +0200

    boilerplate for scenario3

---
+++ scoreboard/static/scoreboard/scenario3/scenario3.js
@@ -0,0 +1,12 @@
+(function() {
+"use strict";
+
+
+App.scenario3_initialize = function() {
+    var box = $('#scenario-box');
+    box.html(App.render('scoreboard/scenario3/scenario3.html'));
+    box.addClass('scenario3');
+};
+
+
+})();
