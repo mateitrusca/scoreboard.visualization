5b3fda969835d29e82ecaf73da22f6a5451f732f
Alex Morega <alex@grep.ro>
2013-01-11 16:35:28 +0200

    table with country colors and labels

--- scoreboard/static/scoreboard/common.js
+++ scoreboard/static/scoreboard/common.js
@@ -54,4 +54,48 @@ App.ChartRouter = Backbone.Router.extend({
 });
 
 
+var country_data = [
+    {code: 'AT',   color: '#AABC66', label: "Austria"},
+    {code: 'BE',   color: '#FD8245', label: "Belgium"},
+    {code: 'BG',   color: '#21FF00', label: "Bulgaria"},
+    {code: 'CY',   color: '#FF5400', label: "Cyprus"},
+    {code: 'CZ',   color: '#1C3FFD', label: "Czech Republic"},
+    {code: 'DE',   color: '#FFC600', label: "Germany"},
+    {code: 'DK',   color: '#45BF55', label: "Denmark"},
+    {code: 'EE',   color: '#0EEAFF', label: "Estonia"},
+    {code: 'EL',   color: '#6A07B0', label: "Greece"},
+    {code: 'ES',   color: '#044C29', label: "Spain"},
+    {code: 'FI',   color: '#7FB2F0', label: "Finland"},
+    {code: 'FR',   color: '#15A9FA', label: "France"},
+    {code: 'HR',   color: '#33EED2', label: "Croatia"},
+    {code: 'HU',   color: '#D40D12', label: "Hungary"},
+    {code: 'IE',   color: '#ADF0F6', label: "Ireland"},
+    {code: 'IS',   color: '#662293', label: "Iceland"},
+    {code: 'IT',   color: '#19BC01', label: "Italy"},
+    {code: 'LT',   color: '#9A24ED', label: "Lithuania"},
+    {code: 'LU',   color: '#D50356', label: "Luxembourg"},
+    {code: 'LV',   color: '#D59AFE', label: "Latvia"},
+    {code: 'MT',   color: '#35478C', label: "Malta"},
+    {code: 'NL',   color: '#FF40F4', label: "Netherlands"},
+    {code: 'NO',   color: '#F70A9B', label: "Norway"},
+    {code: 'PL',   color: '#FF1D23', label: "Poland"},
+    {code: 'PT',   color: '#FFFC00', label: "Portugal"},
+    {code: 'RO',   color: '#1B76FF', label: "Romania"},
+    {code: 'SE',   color: '#436B06', label: "Sweden"},
+    {code: 'SI',   color: '#648E23', label: "Slovenia"},
+    {code: 'SK',   color: '#7DC30F', label: "Slovak Republic"},
+    {code: 'TR',   color: '#9900AB', label: "Turkey"},
+    {code: 'UK',   color: '#D000C4', label: "United Kingdom"},
+    {code: 'EU27', color: '#94090D', label: "European Union - 27 countries"}
+];
+
+
+App.COUNTRY_COLOR = _.object(_(country_data).pluck('code'),
+                             _(country_data).pluck('color'));
+
+
+App.COUNTRY_CODE = _.object(_(country_data).pluck('label'),
+                            _(country_data).pluck('code'));
+
+
 })();
