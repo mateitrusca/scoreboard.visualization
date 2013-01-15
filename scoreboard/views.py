637eb920fd026e9b6c8c387ebd27dda27213097b
Alex Morega <alex@grep.ro>
2013-01-15 18:15:00 +0200

    rename sparql method to all_indicators

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -71,7 +71,7 @@ class ScenarioView(object):
             years[row['indicator']].append(row['year_label'])
 
         indicators = []
-        for row in run_query(self.context['get_all_indicator_labels']):
+        for row in run_query(self.context['all_indicators']):
             indicators.append({
                 'uri': row['indicator'],
                 'label': row['label'],
