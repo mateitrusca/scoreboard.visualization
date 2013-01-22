76ee1c0c8a0ad8e07dc5570bc9556b4905184ff1
Alex Morega <alex@grep.ro>
2013-01-22 14:54:00 +0200

    render map with static data

--- scoreboard/static/scoreboard/scenario5/scenario5.js
+++ scoreboard/static/scoreboard/scenario5/scenario5.js
@@ -5,6 +5,25 @@
 "use strict";
 
 
+App.Scenario5MapView = Backbone.View.extend({
+
+    initialize: function(options) {
+        this.model.on('change', this.filters_changed, this);
+        this.filters_changed();
+    },
+
+    render: function() {
+        this.$el.empty();
+        App.scenario5_map(this.el, {data: demo_data});
+    },
+
+    filters_changed: function() {
+        this.render();
+    }
+
+});
+
+
 App.scenario5_initialize = function() {
     var deps = [
         App.STATIC + '/lib/qtip-2.0.1/jquery.qtip.min.js',
@@ -18,8 +37,36 @@ App.scenario5_initialize = function() {
     $('<link rel="stylesheet">').attr('href', qtip_css).appendTo($('head'));
 
     $script(deps, function() {
-        var container = $('<div>').appendTo($('#scenario-box'))[0];
-        App.scenario5_map(container, {data: demo_data});
+        var box = $('#scenario-box');
+        box.html(App.get_template('scoreboard/scenario5/scenario5.html')());
+        box.addClass('scenario5');
+
+        App.filters = new Backbone.Model();
+        App.router = new App.ChartRouter(App.filters);
+
+        $.getJSON(App.URL + '/filters_data', function(data) {
+            new App.Scenario1FiltersView({
+                model: App.filters,
+                el: $('#the-filters'),
+                filters_data: data
+            });
+
+            App.scenario5_map_view = new App.Scenario5MapView({
+                el: $('#the-map')[0],
+                model: App.filters,
+                indicator_labels: App.get_indicator_labels(data)
+            });
+
+            App.metadata = new App.IndicatorMetadataView({
+                model: App.filters,
+                field: 'indicator',
+                indicators: App.get_indicators(data)
+            });
+            $('#the-metadata').append(App.metadata.el);
+
+        });
+
+        Backbone.history.start();
     });
 };
 
