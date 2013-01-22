ebc34f52abcd9318e246b930ba6869b36afcf4f3
Alex Morega <alex@grep.ro>
2013-01-22 15:24:13 +0200

    rounding utility

--- scoreboard/static/scoreboard/tests.js
+++ scoreboard/static/scoreboard/tests.js
@@ -26,3 +26,30 @@ describe('IndicatorMetadataView', function() {
     });
 
 });
+
+
+describe('Float rounder', function() {
+    "use strict";
+
+    it('should round tiny values to 4 decimals', function() {
+        expect(App.round(0.01234567, 3)).to.equal('0.0123');
+        expect(App.round(0.02345678, 3)).to.equal('0.0235');
+        expect(App.round(0.03456789, 3)).to.equal('0.0346');
+        expect(App.round(0.04000000, 3)).to.equal('0.04');
+    });
+
+    it('should round small values to 2 decimals', function() {
+        expect(App.round(1.234567, 3)).to.equal('1.23');
+        expect(App.round(2.345678, 3)).to.equal('2.35');
+        expect(App.round(3.456789, 3)).to.equal('3.46');
+        expect(App.round(4.000000, 3)).to.equal('4');
+    });
+
+    it('should round large values to 0 decimals', function() {
+        expect(App.round(123.4567, 3)).to.equal('123');
+        expect(App.round(234.5678, 3)).to.equal('235');
+        expect(App.round(345.6789, 3)).to.equal('346');
+        expect(App.round(400.0000, 3)).to.equal('400');
+    });
+
+});
