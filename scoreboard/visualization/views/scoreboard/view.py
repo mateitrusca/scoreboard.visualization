c37042f65c1a335b811e345dbe690fdb9b5aa517
Alex Morega <alex@grep.ro>
2013-02-20 16:23:50 +0200

    migrate the scenarion template

--- scoreboard/visualization/views/scoreboard/view.py
+++ scoreboard/visualization/views/scoreboard/view.py
@@ -4,7 +4,8 @@ import json
 from zope.interface import implements
 from eea.app.visualization.views.view import ViewForm
 from scoreboard.visualization.views.scoreboard.interfaces import IScoreboardView
-from scoreboard.visualization.old.views import render_template
+from scoreboard.visualization.jsapp import jsapp_html
+
 
 class View(ViewForm):
     """ Tile view
@@ -18,10 +19,15 @@ class View(ViewForm):
         """
         return self.data.get('title', '') or self._label
 
-    def get_chart(self):
-        data = {
-            'URL': self.context.absolute_url(),
-            'entry_point': 'App.scenario1_initialize',
-        }
-        data.update(json.loads(self.data['configuration'])['chart-options'])
-        return render_template('scenario.html', **data)
+    @property
+    def entry_point(self):
+        configuration = json.loads(self.data.get('configuration', '{}'))
+        return configuration.get('chart_entry_point',
+                                 'App.scenario1_initialize')
+
+    @property
+    def URL(self):
+        return self.context.absolute_url()
+
+    def jsapp_html(self):
+        return jsapp_html(URL=self.context.absolute_url())
