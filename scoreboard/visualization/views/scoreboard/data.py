35a4ed5f70571a4afa5f2053b935924de8ae74b1
Alex Morega <alex@grep.ro>
2013-02-20 16:41:12 +0200

    move query code to daviz visualization

--- scoreboard/visualization/views/scoreboard/data.py
+++ scoreboard/visualization/views/scoreboard/data.py
@@ -1,6 +1,22 @@
-import simplejson as json
 from collections import defaultdict
-from scoreboard.visualization.old.views import run_query
+import simplejson as json
+from path import path
+from sparql import unpack_row
+from Products.ZSPARQLMethod.Method import ZSPARQLMethod
+
+
+queries = {q['id']: q for q in json.loads(
+    (path(__file__).parent / 'queries.json').bytes())}
+
+
+def run_query(method_name, **kwargs):
+    q = queries[method_name]
+    method_ob = ZSPARQLMethod(q['id'], q['title'], q['endpoint_url'])
+    method_ob.arg_spec = q['arg_spec']
+    method_ob.query = q['query']
+    result = method_ob(**kwargs)
+    return [dict(zip(result.var_names, unpack_row(row)))
+           for row in result.rdfterm_rows]
 
 
 class FiltersView(object):
