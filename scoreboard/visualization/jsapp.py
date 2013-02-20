06b758a61644fb689a061584534f0d5ee954dba5
Alex Morega <alex@grep.ro>
2013-02-20 16:35:46 +0200

    move js app to scoreboard/visualization/jsapp

--- scoreboard/visualization/jsapp.py
+++
@@ -1,3 +0,0 @@
-def jsapp_html(URL):
-    from .old.views import render_template
-    return render_template('jsapp.html', URL=URL)
--- scoreboard/visualization/old/views.py
+++ scoreboard/visualization/old/views.py
@@ -1,32 +1,6 @@
 import simplejson as json
-from zope.interface import Interface
-import jinja2
 from path import path
 from sparql import unpack_row
-
-
-ZSPARQLMETHOD = 'Z SPARQL Method'
-
-
-def get_js_templates(parent=path(__file__).abspath().parent / 'static'):
-    out = {tmpl.name: tmpl.text('utf-8') for tmpl in parent.files('*.html')}
-
-    for folder in parent.dirs():
-        for sub_name, text in get_js_templates(folder).iteritems():
-            out['%s/%s' % (folder.name, sub_name)] = text
-
-    return out
-
-
-jinja_env = jinja2.Environment(
-    loader=jinja2.PackageLoader(__name__, 'templates'))
-jinja_env.globals['STATIC'] = '/++resource++scoreboard'
-jinja_env.globals['get_js_templates'] = get_js_templates
-jinja_env.filters['json'] = lambda v: jinja2.Markup(json.dumps(v))
-
-
-def render_template(name, **kwargs):
-    return jinja_env.get_template(name).render(**kwargs)
 
 
 queries = {q['id']: q for q in json.loads(
