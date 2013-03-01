e9a6d11b3ef4110b0f14934d7bcc4ef2467a4d31
Alex Morega <alex@grep.ro>
2013-03-01 12:40:11 +0200

    unit tests for the new js filters

---
+++ scoreboard/visualization/jsapp/static/scoreboard/filters/tests.js
@@ -0,0 +1,50 @@
+/*global App, Backbone, describe, beforeEach, afterEach, it, expect, sinon */
+/*jshint sub:true */
+
+describe('modular filters', function() {
+    "use strict";
+
+    var url_param = App.testing.url_param;
+
+    describe('IndicatorFilter', function() {
+
+        beforeEach(function() {
+            this.sandbox = sinon.sandbox.create();
+        });
+
+        it('should load options via ajax', function() {
+            this.sandbox.useFakeServer();
+            var server = this.sandbox.server;
+            var view = new App.IndicatorFilter();
+            var options = [{'label': "Option One", 'notation': 'one'},
+                           {'label': "Option Two", 'notation': 'two'}];
+            App.respond_json(server.requests[0], {'options': options});
+            expect(view.options).to.deep.equal(options);
+            expect(url_param(server.requests[0].url, 'dimension')).
+                to.equal('indicator');
+        });
+
+    });
+
+
+    describe('YearFilter', function() {
+
+        beforeEach(function() {
+            this.sandbox = sinon.sandbox.create();
+        });
+
+        it('should load options via ajax', function() {
+            this.sandbox.useFakeServer();
+            var server = this.sandbox.server;
+            var view = new App.YearFilter({model: new Backbone.Model()});
+            var options = [{'label': "Option One", 'notation': 'one'},
+                           {'label': "Option Two", 'notation': 'two'}];
+            App.respond_json(server.requests[0], {'options': options});
+            expect(view.options).to.deep.equal(options);
+            expect(url_param(server.requests[0].url, 'dimension')).
+                to.equal('time-period');
+        });
+
+    });
+
+});
