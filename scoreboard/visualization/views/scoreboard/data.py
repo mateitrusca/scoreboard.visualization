873b38aadf3aa0247a684790a8a69791a01f301a
Alex Morega <alex@grep.ro>
2013-03-01 16:48:34 +0200

    ajax view for fetching data points

--- scoreboard/visualization/views/scoreboard/data.py
+++ scoreboard/visualization/views/scoreboard/data.py
@@ -64,6 +64,15 @@ class FiltersView(object):
         self.request.RESPONSE.setHeader("Content-Type", "application/json")
         return json.dumps({'options': options}, indent=2, sort_keys=True)
 
+    def data(self):
+        cube = Cube(SPARQL_ENDPOINT, DATASET)
+        form = dict(self.request.form)
+        columns = form.pop('columns').split(',')
+        filters = sorted(form.items())
+        rows = list(cube.get_data(columns=columns, filters=filters))
+        self.request.RESPONSE.setHeader("Content-Type", "application/json")
+        return json.dumps({'rows': rows}, indent=2, sort_keys=True)
+
 
 class DataView(object):
 
--- scoreboard/visualization/views/scoreboard/tests/ajax_test.py
+++ scoreboard/visualization/views/scoreboard/tests/ajax_test.py
@@ -9,6 +9,13 @@ def _filter_options_query(form):
     return json.loads(filters_view.filter_options())
 
 
+def _data_query(form):
+    from scoreboard.visualization.views.scoreboard import data
+    filters_view = data.FiltersView()
+    filters_view.request = Mock(form=form)
+    return json.loads(filters_view.data())
+
+
 def test_all_indicator_values():
     with patch('scoreboard.visualization.views.scoreboard.data.Cube') as Cube:
         Cube.return_value.get_dimension_options.return_value = [
@@ -40,3 +47,37 @@ def test_filters_passed_on_to_query():
     cube_call = Cube.return_value.get_dimension_options.mock_calls[0]
     assert cube_call == call('ref-area', [('indicator', 'h_iacc'),
                                           ('time-period', '2002')])
+
+
+def test_data_query_sends_filters_and_columns():
+    with patch('scoreboard.visualization.views.scoreboard.data.Cube') as Cube:
+        _data_query({
+            'columns': 'time-period,ref-area,value',
+            'indicator': 'i_bfeu',
+            'breakdown': 'IND_TOTAL',
+            'unit-measure': 'pc_ind',
+        })
+    cube_call = Cube.return_value.get_data.mock_calls[0]
+    assert cube_call == call(columns=['time-period', 'ref-area', 'value'],
+                             filters=[('breakdown', 'IND_TOTAL'),
+                                      ('indicator', 'i_bfeu'),
+                                      ('unit-measure', 'pc_ind')])
+
+
+def test_data_query_returns_rows():
+    rows = [{'time-period': '2011', 'ref-area': 'IE', 'value': 0.2222},
+            {'time-period': '2010', 'ref-area': 'PT', 'value': 0.0609}]
+    with patch('scoreboard.visualization.views.scoreboard.data.Cube') as Cube:
+        Cube.return_value.get_data.return_value = iter(rows)
+        res = _data_query({
+            'columns': 'time-period,ref-area,value',
+            'indicator': 'i_bfeu',
+            'breakdown': 'IND_TOTAL',
+            'unit-measure': 'pc_ind',
+        })
+    cube_call = Cube.return_value.get_data.mock_calls[0]
+    assert cube_call == call(columns=['time-period', 'ref-area', 'value'],
+                             filters=[('breakdown', 'IND_TOTAL'),
+                                      ('indicator', 'i_bfeu'),
+                                      ('unit-measure', 'pc_ind')])
+    assert res == {'rows': rows}
