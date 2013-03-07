c9c2d03d1e2c60e0e8775a6ce8c9736f32b819da
Alex Morega <alex@grep.ro>
2013-03-07 18:36:48 +0200

    move sparql templates to separate files

--- scoreboard/visualization/data/cube.py
+++ scoreboard/visualization/data/cube.py
@@ -8,180 +8,11 @@ SPARQL_DEBUG = bool(os.environ.get('SPARQL_DEBUG') == 'on')
 
 logger = logging.getLogger(__name__)
 
+sparql_env = jinja2.Environment(loader=jinja2.PackageLoader(__name__))
+
 
 class QueryError(Exception):
     pass
-
-
-sparql_templates = {}
-
-
-sparql_templates['bits'] = """\
-{%- set group_dimensions = ['indicator-group', 'breakdown-group'] -%}
-{% macro one_filter(prefix, dimension_code, option_code) %}
-  ?observation
-    ?{{ prefix }}_dimension ?{{ prefix }}_option .
-
-  {%- if dimension_code.value in group_dimensions %}
-  ?{{ prefix }}_dimension
-    dad-prop:grouped-using ?{{ prefix }}_dimension_group .
-  ?{{ prefix }}_dimension_group
-    skos:notation ?{{ prefix }}_dimension_group_code .
-  ?{{ prefix }}_option
-    dad-prop:membership [
-      dad-prop:member-of ?{{ prefix }}_option_group ] .
-  ?{{ prefix }}_option_group
-    skos:notation ?{{ prefix }}_option_group_code .
-  FILTER (
-    ?{{ prefix }}_dimension_group_code = {{ dimension_code.n3() }} &&
-    ?{{ prefix }}_option_group_code = {{ option_code.n3() }}
-  )
-
-  {%- else %}
-  ?{{ prefix }}_dimension
-    skos:notation ?{{ prefix }}_dimension_code .
-  ?{{ prefix }}_option
-    skos:notation ?{{ prefix }}_option_code .
-  FILTER (
-    ?{{ prefix }}_dimension_code = {{ dimension_code.n3() }} &&
-    ?{{ prefix }}_option_code = {{ option_code.n3() }}
-  )
-
-  {%- endif %}
-{% endmacro %}
-"""
-
-
-sparql_templates['dimensions'] = """\
-PREFIX qb: <http://purl.org/linked-data/cube#>
-PREFIX dad-prop: <http://semantic.digital-agenda-data.eu/def/property/>
-SELECT DISTINCT ?notation, ?group_notation WHERE {
-  ?dataset
-    a qb:DataSet ;
-    qb:structure ?structure .
-  ?structure
-    qb:component ?componentSpec .
-  ?componentSpec
-    qb:dimension ?dimension ;
-    qb:order ?componentSpecOrder .
-  ?dimension
-    skos:notation ?notation .
-  OPTIONAL {
-    ?dimension
-      dad-prop:grouped-using ?group .
-    ?group
-      skos:notation ?group_notation .
-  }
-  FILTER (
-    ?dataset = {{ dataset.n3() }}
-  )
-}
-ORDER BY ?componentSpecOrder
-LIMIT 100
-"""
-
-
-sparql_templates['dimension_options'] = """\
-{%- set group_dimensions = ['indicator-group', 'breakdown-group'] -%}
-{%- set short_label_required = ['breakdown-group', 'breakdown', 'unit-measure'] -%}
-{%- from 'bits' import one_filter -%}
-PREFIX qb: <http://purl.org/linked-data/cube#>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX dad-prop: <http://semantic.digital-agenda-data.eu/def/property/>
-SELECT DISTINCT ?uri, ?notation, ?label, ?short_label WHERE {
-  ?dataset
-    a qb:DataSet .
-  ?observation
-    a qb:Observation ;
-    qb:dataSet ?dataset ;
-    ?dimension ?option .
-  FILTER (
-    ?dataset = {{ dataset.n3() }}
-  )
-
-{%- for f_dimension_code, f_option_code in filters %}
-  {{ one_filter('filter_%d' % loop.index, f_dimension_code, f_option_code) }}
-{%- endfor %}
-
-  {%- if dimension_code.value in group_dimensions %}
-  ?dimension
-    dad-prop:grouped-using ?dimension_group .
-  ?dimension_group
-    skos:notation ?dimension_code .
-  ?option
-    dad-prop:membership [
-      dad-prop:member-of ?option_group ] .
-  ?option_group
-    skos:notation ?notation ;
-    {% if dimension_code.value in short_label_required %}
-        skos:altLabel ?short_label ;
-    {% endif %}
-    skos:prefLabel ?label .
-  FILTER (
-    ?dimension_code = {{ dimension_code.n3() }} &&
-    ?option_group = ?uri
-  )
-
-  {%- else %}
-  ?dimension
-    skos:notation ?dimension_code .
-  ?option
-    skos:notation ?notation ;
-    {% if dimension_code.value in short_label_required %}
-        skos:altLabel ?short_label ;
-    {% endif %}
-    skos:prefLabel ?label .
-  FILTER (
-    ?dimension_code = {{ dimension_code.n3() }} &&
-    ?option = ?uri
-  )
-
-  {%- endif %}
-}
-LIMIT 100
-"""
-
-
-sparql_templates['data'] = """\
-{%- from 'bits' import one_filter -%}
-PREFIX qb: <http://purl.org/linked-data/cube#>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX sdmx-measure: <http://purl.org/linked-data/sdmx/2009/measure#>
-PREFIX dad-prop: <http://semantic.digital-agenda-data.eu/def/property/>
-SELECT DISTINCT {% for f in columns %} {%- set n = loop.index -%}
-                ?col{{n}}, {% endfor -%} ?value WHERE {
-  ?dataset
-    a qb:DataSet .
-  ?observation
-    a qb:Observation ;
-    qb:dataSet ?dataset ;
-    {%- for c in columns %} {%- set n = loop.index %}
-    ?col{{n}}_dimension ?col{{n}}_option ;
-    {%- endfor %}
-    sdmx-measure:obsValue ?value .
-
-{%- for f_dimension_code, f_option_code in filters %}
-  {{ one_filter('filter_%d' % loop.index, f_dimension_code, f_option_code) }}
-{%- endfor %}
-
-  {%- for c in columns %} {%- set n = loop.index %}
-  ?col{{n}}_dimension
-    skos:notation ?col{{n}}_dimension_code .
-  ?col{{n}}_option
-    skos:notation ?col{{n}} .
-  {%- endfor %}
-  FILTER (
-    {%- for c in columns %} {%- set n = loop.index %}
-    ?col{{n}}_dimension_code = {{ c.n3() }} &&
-    {%- endfor %}
-    ?dataset = {{ dataset.n3() }}
-  )
-}
-LIMIT 1000
-"""
-
-
-sparql_env = jinja2.Environment(loader=jinja2.DictLoader(sparql_templates))
 
 
 class Cube(object):
@@ -207,13 +38,13 @@ class Cube(object):
             return rv
 
     def get_dimensions(self):
-        query = sparql_env.get_template('dimensions').render(**{
+        query = sparql_env.get_template('dimensions.sparql').render(**{
             'dataset': self.dataset,
         })
         return list(self._execute(query, as_dict=True))
 
     def get_dimension_options(self, dimension, filters=[]):
-        query = sparql_env.get_template('dimension_options').render(**{
+        query = sparql_env.get_template('dimension_options.sparql').render(**{
             'dataset': self.dataset,
             'dimension_code': sparql.Literal(dimension),
             'filters': [(sparql.Literal(f), sparql.Literal(v))
@@ -223,7 +54,7 @@ class Cube(object):
 
     def get_data(self, columns, filters):
         assert columns[-1] == 'value', "Last column must be 'value'"
-        query = sparql_env.get_template('data').render(**{
+        query = sparql_env.get_template('data.sparql').render(**{
             'dataset': self.dataset,
             'columns': [sparql.Literal(c) for c in columns[:-1]],
             'filters': [(sparql.Literal(f), sparql.Literal(v))
