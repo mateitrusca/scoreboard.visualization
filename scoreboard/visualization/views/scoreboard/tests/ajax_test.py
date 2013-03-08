183f3ee0dd5c7ac7e34adfeb76ac2829df9525c5
Alex Morega <alex@grep.ro>
2013-03-08 19:00:35 +0200

    move mocking of Cube to fixture

--- scoreboard/visualization/views/scoreboard/tests/ajax_test.py
+++ scoreboard/visualization/views/scoreboard/tests/ajax_test.py
@@ -1,5 +1,6 @@
 from mock import Mock, patch, call
 import simplejson as json
+import pytest
 
 
 def _dimension_values_query(form):
@@ -14,66 +15,68 @@ def _data_query(form):
     return json.loads(view.datapoints())
 
 
-def test_all_indicator_values():
-    with patch('scoreboard.visualization.views.scoreboard.data.Cube') as Cube:
-        Cube.return_value.get_dimension_options.return_value = [
-            {'label': 'indicator one', 'notation': 'one'},
-            {'label': 'indicator two', 'notation': 'two'},
-        ]
-        res = _dimension_values_query({'dimension': 'indicator'})
+@pytest.fixture()
+def mock_cube(request):
+    p = patch('scoreboard.visualization.views.scoreboard.data.Cube')
+    request.addfinalizer(p.stop)
+    return p.start().return_value
+
+
+def test_all_indicator_values(mock_cube):
+    mock_cube.get_dimension_options.return_value = [
+        {'label': 'indicator one', 'notation': 'one'},
+        {'label': 'indicator two', 'notation': 'two'},
+    ]
+    res = _dimension_values_query({'dimension': 'indicator'})
     assert {'label': 'indicator one', 'notation': 'one'} in res['options']
     assert {'label': 'indicator two', 'notation': 'two'} in res['options']
 
 
-def test_single_filter_passed_on_to_query():
-    with patch('scoreboard.visualization.views.scoreboard.data.Cube') as Cube:
-        _dimension_values_query({
-            'dimension': 'ref-area',
-            'time-period': '2002',
-        })
-    cube_call = Cube.return_value.get_dimension_options.mock_calls[0]
+def test_single_filter_passed_on_to_query(mock_cube):
+    _dimension_values_query({
+        'dimension': 'ref-area',
+        'time-period': '2002',
+    })
+    cube_call = mock_cube.get_dimension_options.mock_calls[0]
     assert cube_call == call('ref-area', [('time-period', '2002')])
 
 
-def test_filters_passed_on_to_query():
-    with patch('scoreboard.visualization.views.scoreboard.data.Cube') as Cube:
-        _dimension_values_query({
-            'dimension': 'ref-area',
-            'time-period': '2002',
-            'indicator': 'h_iacc',
-        })
-    cube_call = Cube.return_value.get_dimension_options.mock_calls[0]
+def test_filters_passed_on_to_query(mock_cube):
+    _dimension_values_query({
+        'dimension': 'ref-area',
+        'time-period': '2002',
+        'indicator': 'h_iacc',
+    })
+    cube_call = mock_cube.get_dimension_options.mock_calls[0]
     assert cube_call == call('ref-area', [('indicator', 'h_iacc'),
                                           ('time-period', '2002')])
 
 
-def test_data_query_sends_filters_and_columns():
-    with patch('scoreboard.visualization.views.scoreboard.data.Cube') as Cube:
-        _data_query({
-            'columns': 'time-period,ref-area,value',
-            'indicator': 'i_bfeu',
-            'breakdown': 'IND_TOTAL',
-            'unit-measure': 'pc_ind',
-        })
-    cube_call = Cube.return_value.get_data.mock_calls[0]
+def test_data_query_sends_filters_and_columns(mock_cube):
+    _data_query({
+        'columns': 'time-period,ref-area,value',
+        'indicator': 'i_bfeu',
+        'breakdown': 'IND_TOTAL',
+        'unit-measure': 'pc_ind',
+    })
+    cube_call = mock_cube.get_data.mock_calls[0]
     assert cube_call == call(columns=['time-period', 'ref-area', 'value'],
                              filters=[('breakdown', 'IND_TOTAL'),
                                       ('indicator', 'i_bfeu'),
                                       ('unit-measure', 'pc_ind')])
 
 
-def test_data_query_returns_rows():
+def test_data_query_returns_rows(mock_cube):
     rows = [{'time-period': '2011', 'ref-area': 'IE', 'value': 0.2222},
             {'time-period': '2010', 'ref-area': 'PT', 'value': 0.0609}]
-    with patch('scoreboard.visualization.views.scoreboard.data.Cube') as Cube:
-        Cube.return_value.get_data.return_value = iter(rows)
-        res = _data_query({
-            'columns': 'time-period,ref-area,value',
-            'indicator': 'i_bfeu',
-            'breakdown': 'IND_TOTAL',
-            'unit-measure': 'pc_ind',
-        })
-    cube_call = Cube.return_value.get_data.mock_calls[0]
+    mock_cube.get_data.return_value = iter(rows)
+    res = _data_query({
+        'columns': 'time-period,ref-area,value',
+        'indicator': 'i_bfeu',
+        'breakdown': 'IND_TOTAL',
+        'unit-measure': 'pc_ind',
+    })
+    cube_call = mock_cube.get_data.mock_calls[0]
     assert cube_call == call(columns=['time-period', 'ref-area', 'value'],
                              filters=[('breakdown', 'IND_TOTAL'),
                                       ('indicator', 'i_bfeu'),
