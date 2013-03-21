7a05e9210120b988e62f375577ab651cf77b70af
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-03-21 10:39:43 +0200

    use ScenarioChartView with scenario2

--- scoreboard/visualization/jsapp/static/scoreboard/scenario2/scenario2.js
+++ scoreboard/visualization/jsapp/static/scoreboard/scenario2/scenario2.js
@@ -55,65 +55,6 @@ App.Scenario2FiltersView = Backbone.View.extend({
 });
 
 
-App.Scenario2ChartView = Backbone.View.extend({
-
-    className: "highcharts-chart",
-
-    initialize: function(options) {
-        this.country_labels = options['country_labels'];
-        this.indicator_labels = options['indicator_labels'];
-        this.model.on('change', this.filters_changed, this);
-    },
-
-    render: function() {
-        if(this.data) {
-            App.scenario2_chart(this.el, this.data);
-        }
-    },
-
-    filters_changed: function() {
-        var view = this;
-        var args = this.model.toJSON();
-        if(! (args['indicator'] && args['country'])) {
-            return;
-        }
-
-        var countries = args['country'];
-        var requests = _(countries).map(function(country_uri) {
-            var data_ajax = $.get(App.URL + '/data', {
-                'method': 'series_indicator_country',
-                'indicator': args['indicator'],
-                'country': country_uri
-            });
-            return data_ajax;
-        });
-        var country_labels = this.country_labels;
-
-        var ajax_calls = $.when.apply($, requests);
-        ajax_calls.done(function() {
-            var responses = (requests.length == 1) ? _([arguments])
-                                                   : _(arguments).toArray();
-            var series = _(responses).map(function(resp, n) {
-                return {
-                    'label': country_labels[countries[n]],
-                    'data': resp[0]
-                };
-            });
-            view.data = {
-                'series': series,
-                'indicator_label': view.indicator_labels[args['indicator']],
-                'credits': {
-                    'href': 'http://ec.europa.eu/digital-agenda/en/graphs/',
-                    'text': 'European Commission, Digital Agenda Scoreboard'
-                }
-            };
-            view.render();
-        });
-    }
-
-});
-
-
 App.scenario2_initialize = function() {
 
     var box = $('#scenario-box');
@@ -130,10 +71,40 @@ App.scenario2_initialize = function() {
             filters_data: data
         });
 
-        App.scenario2_chart_view = new App.Scenario2ChartView({
+        App.scenario2_chart_view = new App.ScenarioChartView({
             model: App.filters,
-            country_labels: App.get_country_labels(data),
-            indicator_labels: App.get_indicator_labels(data)
+            schema: {
+                filters: [
+                    {type: 'select',
+                     name: 'indicator',
+                     label: 'Select one indicator',
+                     dimension: 'indicator',
+                     constraints: { }
+                    },
+                    {type: 'select',
+                     name: 'country',
+                     label: 'Select the countries',
+                     dimension: 'breakdown',
+                     constraints: {
+                         'indicator': 'indicator'
+                     }},
+                ]
+            },
+            datasource: {
+                data_preparation: {
+                    group: {
+                        filter_name: 'country',
+                        labels: App.get_country_labels(data),
+                    }
+                },
+                rel_url: '/data',
+                extra_args: [
+                    ['method', 'series_indicator_country'],
+                    ['fields', 'ref-area,value'],
+                    ['rev', App.DATA_REVISION]
+                ]
+            },
+            scenario_chart: App.scenario2_chart
         });
         $('#the-chart').append(App.scenario2_chart_view.el);
         App.scenario2_chart_view.filters_changed();
