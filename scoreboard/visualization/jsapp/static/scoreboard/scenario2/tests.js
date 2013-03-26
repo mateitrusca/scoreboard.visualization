5a8a1911b864b95a8c5a2c1e896569c7e7f111b6
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-03-26 12:44:43 +0200

    add tests

--- scoreboard/visualization/jsapp/static/scoreboard/scenario2/tests.js
+++ scoreboard/visualization/jsapp/static/scoreboard/scenario2/tests.js
@@ -1,70 +1,6 @@
 /*global App, Backbone, describe, beforeEach, afterEach, it, expect, sinon */
 /*jshint sub:true */
 
-describe('Scenario2FiltersView', function() {
-    "use strict";
-
-    var $ = App.jQuery;
-    var choose = App.testing.choose_option;
-
-    beforeEach(function() {
-        this.filters_data = {
-            'indicators': [{'uri': 'ind1'}, {'uri': 'ind2'}],
-            'countries': [{'uri': 'fr'}, {'uri': 'dk'}, {'uri': 'es'}]
-        };
-        this.model = new Backbone.Model();
-        this.view = new App.Scenario2FiltersView({
-            model: this.model,
-            filters_data: this.filters_data
-        });
-    });
-
-    describe('indicators selector', function() {
-
-        it('should update model', function() {
-            choose(this.view.$el.find('select[name=indicator]'), 'ind2');
-            expect(this.model.get('indicator')).to.equal('ind2');
-        });
-
-        it('should select the current indicator', function() {
-            this.model.set({'indicator': 'ind2'});
-            choose(this.view.$el.find('select[name=indicator]'), 'ind2');
-            var option_two = this.view.$el.find('select[name=indicator] ' +
-                                                'option[value=ind2]');
-            expect(option_two.attr('selected')).to.equal('selected');
-        });
-
-        it('should make an initial selection', function() {
-            expect(this.model.get('indicator')).to.equal('ind1');
-        });
-
-    });
-
-    describe('countries selector', function() {
-
-        it('should update model', function() {
-            choose(this.view.$el.find('select[name=country]'), 'dk');
-            expect(this.model.get('country')).to.deep.equal(['dk']);
-        });
-
-        it('should update model with multiple values', function() {
-            choose(this.view.$el.find('select[name=country]'), 'dk');
-            choose(this.view.$el.find('select[name=country]'), 'es');
-            this.view.$el.filter('select[name=country]').change();
-            expect(this.model.get('country')).to.deep.equal(['dk', 'es']);
-        });
-
-        it('should select the current values', function() {
-            this.model.set({'country': ['dk', 'es']});
-            var sel = this.view.$el.find('select[name=country]');
-            expect(sel.val()).to.deep.equal(['dk', 'es']);
-        });
-
-    });
-
-});
-
-
 describe('Scenario2ChartView', function() {
     "use strict";
 
@@ -73,13 +9,46 @@ describe('Scenario2ChartView', function() {
         this.scenario2_chart = this.sandbox.stub(App, 'scenario2_chart');
 
         this.model = new Backbone.Model();
-        this.chart = new App.Scenario2ChartView({
+        this.chart = new App.ScenarioChartView({
             model: this.model,
-            country_labels: {
-                'http://data.lod2.eu/scoreboard/country/Denmark': "Denmark",
-                'http://data.lod2.eu/scoreboard/country/Spain': "Spain"
+            schema: {
+                filters: [
+                    {type: 'select',
+                     name: 'indicator',
+                     label: 'Select one indicator',
+                     dimension: 'indicator',
+                     constraints: { }
+                    },
+                    {type: 'select',
+                     name: 'countries',
+                     label: 'Select one indicator',
+                     dimension: 'ref-area',
+                     constraints: {
+                         'indicator': 'indicator'
+                     }},
+                ]
             },
-            indicator_labels: {'ind1': "The Label!"}
+            datasource: {
+                data_preparation: {
+                    group: {
+                        filter_name: 'countries',
+                        labels: {
+                            'BE': "Belgium",
+                            'DK': "Denmark",
+                            'ES': "Spain"
+                        }
+                    }
+                },
+                rel_url: '/source_view',
+                extra_args: [
+                    ['fields', 'dimension1,value1'],
+                    ['rev', App.DATA_REVISION]
+                ]
+            },
+            meta_labels: [
+                { targets: ['label'], filter_name: 'indicator', type: 'label' },
+            ],
+            scenario_chart: this.scenario2_chart
         });
     });
 
@@ -92,38 +61,45 @@ describe('Scenario2ChartView', function() {
         var server = this.sandbox.server;
         this.model.set({
             'indicator': 'ind1',
-            'country': ['http://data.lod2.eu/scoreboard/country/Denmark']
+            'countries': ['BE']
         });
-
         var url = server.requests[0].url;
-        expect(url).to.have.string(App.URL + '/data?');
+        expect(url).to.have.string(App.URL + '/source_view?');
         var url_param = App.testing.url_param;
-        expect(url_param(url, 'method')).to.equal('series_indicator_country');
         expect(url_param(url, 'indicator')).to.equal('ind1');
-        expect(url_param(url, 'country')).to.equal(
-            'http://data.lod2.eu/scoreboard/country/Denmark');
+        expect(url_param(url, 'fields')).to.equal('dimension1,value1');
+        expect(url_param(url, 'ref-area')).to.equal('BE');
     });
 
-    it('should render chart with the data is received', function() {
+    it('should render chart with the data received', function() {
         this.sandbox.useFakeServer();
         var server = this.sandbox.server;
         this.model.set({
             'indicator': 'ind1',
-            'country': ['http://data.lod2.eu/scoreboard/country/Denmark']
+            'countries': ['BE']
         });
 
-        var data_dk = [{'year': "2010", 'value': 0.18},
-                       {'year': "2011", 'value': 0.14}];
-        App.respond_json(server.requests[0], data_dk);
-
+        var data_be = {datapoints: [
+                        {'year': "2010", 'value': 0.18},
+                        {'year': "2011", 'value': 0.14}]
+                      };
+        var country_labels = {options: [{
+                label: 'Belgium',
+                notation: 'BE'
+            }]
+        }
+        App.respond_json(server.requests[0], data_be);
+        App.respond_json(server.requests[1], country_labels);
+        App.respond_json(server.requests[2],
+            {'label': 'normal_label', 'short_label': 'short_label'});
         var container = this.chart.el;
         expect(this.scenario2_chart.calledOnce).to.equal(true);
         var call_args = this.scenario2_chart.getCall(0).args;
         expect(call_args[0]).to.equal(container);
-        expect(call_args[1]['series']).to.deep.equal([
-            {'label': "Denmark", 'data': data_dk}
-        ]);
-        expect(call_args[1]['indicator_label']).to.equal("The Label!");
+        expect(call_args[1]['series'][0]['data']).to.deep.equal(
+            data_be['datapoints']
+        );
+        expect(call_args[1]['series'][0]['label']).to.equal("Belgium");
     });
 
     it('should render chart with multiple countries', function() {
@@ -131,22 +107,29 @@ describe('Scenario2ChartView', function() {
         var server = this.sandbox.server;
         this.model.set({
             'indicator': 'ind1',
-            'country': ['http://data.lod2.eu/scoreboard/country/Denmark',
-                        'http://data.lod2.eu/scoreboard/country/Spain']
+            'countries': ['DK', 'ES']
         });
 
-        var data_dk = [{'year': "2010", 'value': 0.18},
-                       {'year': "2011", 'value': 0.14}];
-        var data_es = [{'year': "2011", 'value': 0.22},
-                       {'year': "2012", 'value': 0.26}];
+        var data_dk = {datapoints: [{'year': "2010", 'value': 0.18},
+                       {'year': "2011", 'value': 0.14}]};
+        var data_es = {datapoints: [{'year': "2011", 'value': 0.22},
+                       {'year': "2012", 'value': 0.26}]};
+        var country_labels = {options: [
+            { label: 'Denmark',
+              notation: 'DK' },
+            { label: 'Spain',
+              notation: 'ES' },
+        ]};
 
         App.respond_json(server.requests[0], data_dk);
         App.respond_json(server.requests[1], data_es);
-
+        App.respond_json(server.requests[2], country_labels);
+        App.respond_json(server.requests[3],
+            {'label': 'normal_label', 'short_label': 'short_label'});
         var call_args = this.scenario2_chart.getCall(0).args;
         expect(call_args[1]['series']).to.deep.equal([
-            {'label': "Denmark", 'data': data_dk},
-            {'label': "Spain", 'data': data_es}
+            {'label': "Denmark", 'data': data_dk['datapoints']},
+            {'label': "Spain", 'data': data_es['datapoints']}
         ]);
     });
 
