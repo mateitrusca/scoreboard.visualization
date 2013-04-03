dfa2928cd9fb91086bfebb3833a00bae4578ba54
Alex Morega <alex@grep.ro>
2013-04-03 16:47:52 +0300

    remove old filter implementations

--- scoreboard/visualization/jsapp/scenario1/scenario1.js
+++ scoreboard/visualization/jsapp/scenario1/scenario1.js
@@ -5,64 +5,6 @@
 "use strict";
 
 
-App.Scenario1FiltersView = Backbone.View.extend({
-
-    template: App.get_template('scenario1/filters.html'),
-
-    events: {
-        'change select': 'update_filters',
-        'change input[name=year]': 'update_filters'
-    },
-
-    initialize: function(options) {
-        this.filters_data = JSON.stringify(options['filters_data']);
-        if(! this.model.get('indicator')) {
-            var first_indicator = options['filters_data']['indicators'][0];
-            this.model.set({
-                'indicator': first_indicator['uri'],
-                'year': first_indicator['years'][0]
-            });
-        }
-        this.model.on('change', this.render, this);
-        this.render();
-    },
-
-    get_options: function(value) {
-        var data = JSON.parse(this.filters_data);
-        var index = App.index_by(data['indicators'], 'uri');
-        var indicator = index[value['indicator']];
-
-        if(indicator) {
-            data['years'] = _(indicator['years']).map(function(year) {
-                return {
-                    'value': year,
-                    'selected': (year == value['year'])
-                };
-            });
-            indicator['selected'] = true;
-        }
-
-        return data;
-    },
-
-    render: function() {
-        var options = this.get_options(this.model.toJSON());
-        this.$el.html(this.template(options));
-    },
-
-    update_filters: function() {
-        var new_value = {'indicator': this.$el.find('select').val()};
-        var year = this.$el.find('input[name=year]:checked').val();
-        var options = this.get_options(new_value);
-        var available_years = _(_(options['years']).pluck('value'));
-        if(! available_years.contains(year)) { year = null; }
-        new_value['year'] = year;
-        this.model.set(new_value);
-    }
-
-});
-
-
 App.scenario1_filters_schema = {
     filters: [
         {type: 'select',
