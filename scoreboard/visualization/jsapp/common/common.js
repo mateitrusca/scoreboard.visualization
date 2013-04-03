813720efeecabcd7367a040e54c7f61a8874b8e8
Alex Morega <alex@grep.ro>
2013-04-03 18:06:51 +0300

    join remaining `scoreboard` code into `common`

--- scoreboard/visualization/jsapp/common/common.js
+++ scoreboard/visualization/jsapp/common/common.js
@@ -49,4 +49,17 @@ App.COUNTRY_NAME = _.object(_(country_data).pluck('code'),
                             _(country_data).pluck('label'));
 
 
+App.index_by = function(list, prop) {
+    return _.object(_(list).pluck(prop), list);
+};
+
+
+App.round = function(value, precision) {
+    var magnitude = Math.floor(Math.log(value) / Math.LN10);
+    var decimals = precision - magnitude - 1;
+    var power = Math.pow(10, decimals);
+    return '' + Math.round(value * power) / power;
+};
+
+
 })(App.jQuery);
