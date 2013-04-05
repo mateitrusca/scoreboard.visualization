cc231845ed759f5eeedcab5bcd7ee85fcb8d2046
Alex Morega <alex@grep.ro>
2013-04-05 15:23:35 +0300

    view that reads/writes form hidden input

--- scoreboard/visualization/jsapp/editor/editor.js
+++ scoreboard/visualization/jsapp/editor/editor.js
@@ -5,7 +5,26 @@
 "use strict";
 
 
+App.EditForm = Backbone.View.extend({
+
+    initialize: function(options) {
+        this.model.set(JSON.parse(this.$el.val()));
+        this.model.on('change', this.save, this);
+    },
+
+    save: function() {
+        this.$el.val(JSON.stringify(this.model, null, 2));  // indent 2 spaces
+    }
+
+});
+
+
 App.create_editor = function(form) {
+    var configuration = new Backbone.Model();
+    App.editor_form = new App.EditForm({
+        model: configuration,
+        el: $(form).find('input[name=configuration]')[0]
+    });
     $(form).append('hello editor!');
 };
 
