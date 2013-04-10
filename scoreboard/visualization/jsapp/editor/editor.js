15dec89c8cb4c2c4b713e0be586f240ba916ad21
Alex Morega <alex@grep.ro>
2013-04-10 12:11:49 +0200

    `el` is the entire form

--- scoreboard/visualization/jsapp/editor/editor.js
+++ scoreboard/visualization/jsapp/editor/editor.js
@@ -8,12 +8,13 @@
 App.EditForm = Backbone.View.extend({
 
     initialize: function(options) {
-        this.model.set(JSON.parse(this.$el.val()));
+        this.input = this.$el.find('input[name=configuration]');
+        this.model.set(JSON.parse(this.input.val()));
         this.model.on('change', this.save, this);
     },
 
     save: function() {
-        this.$el.val(JSON.stringify(this.model, null, 2));  // indent 2 spaces
+        this.input.val(JSON.stringify(this.model, null, 2));  // indent 2 spaces
     }
 
 });
@@ -23,7 +24,7 @@ App.create_editor = function(form) {
     var configuration = new Backbone.Model();
     App.editor_form = new App.EditForm({
         model: configuration,
-        el: $(form).find('input[name=configuration]')[0]
+        el: form
     });
     $(form).append('hello editor!');
 };
