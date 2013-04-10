097d2aae92e34a2ce5c406cb129d9711be4847a0
Alex Morega <alex@grep.ro>
2013-04-10 12:49:12 +0200

    select chart type

--- scoreboard/visualization/jsapp/editor/editor.js
+++ scoreboard/visualization/jsapp/editor/editor.js
@@ -26,6 +26,10 @@ App.Editor = Backbone.View.extend({
 
     template: App.get_template('editor/editor.html'),
 
+    events: {
+        'change': 'save'
+    },
+
     steps: [
         {label: "Chart Type"},
         {label: "Filters"},
@@ -35,10 +39,26 @@ App.Editor = Backbone.View.extend({
         {label: "Annotations"}
     ],
 
+    chart_types: [
+        {label: "Bar", disabled: true},
+        {label: "Line", value: 'lines'},
+        {label: "Column", value: 'columns'},
+        {label: "Scatterplot", value: 'scatter'},
+        {label: "Map", value: 'map'},
+        {label: "Country Profile", disabled: true}
+    ],
+
     initialize: function(options) {
         this.$el.html(this.template({
-            steps: this.steps
+            steps: this.steps,
+            chart_types: this.chart_types
         }));
+    },
+
+    save: function() {
+        this.model.set({
+            chart_type: this.$el.find('[name=chart-type]:checked').val()
+        });
     }
 
 });
--- scoreboard/visualization/jsapp/editor/tests.js
+++ scoreboard/visualization/jsapp/editor/tests.js
@@ -4,4 +4,13 @@
 describe('ScenarioEditor', function() {
     "use strict";
 
+    var testing = App.testing;
+
+    it('should select chart type', function() {
+        var model = new Backbone.Model();
+        var editor = new App.Editor({model: model});
+        testing.choose_radio(editor.$el.find('[name=chart-type]'), 'columns');
+        expect(model.get('chart_type')).to.equal('columns');
+    });
+
 });
