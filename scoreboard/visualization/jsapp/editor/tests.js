f7878bfeb62998db1b0b6b9f9ff6a5135fad3916
Alex Morega <alex@grep.ro>
2013-04-13 12:53:32 +0300

    test for load_value after model changes

--- scoreboard/visualization/jsapp/editor/tests.js
+++ scoreboard/visualization/jsapp/editor/tests.js
@@ -107,6 +107,23 @@ describe('FacetsEditor', function() {
         expect(select.val()).to.equal('data-column');
     });
 
+    it('should reload from model on demand', function() {
+        var model = new Backbone.Model({
+            facets: [{name: 'time-period', type: 'data-column'}]
+        });
+        var view = new App.FacetsEditor({model: model, el: this.box});
+        App.respond_json(this.sandbox.server.requests[0], [
+            {type_label: 'dimension', notation: 'time-period'}
+        ]);
+        var read_current_selection = function() {
+            return view.$el.find('select[name="type"]').val();
+        };
+        expect(read_current_selection()).to.equal('data-column');
+        model.set('facets', [{name: 'time-period', type: 'all-values'}]);
+        view.load_value();
+        expect(read_current_selection()).to.equal('all-values');
+    });
+
 });
 
 
