d14a2b1ae2e7207c7aaf7571371cd72f5c39fd6f
Alex Morega <alex@grep.ro>
2013-04-13 13:21:35 +0300

    fix indentation

--- scoreboard/visualization/jsapp/editor/tests.js
+++ scoreboard/visualization/jsapp/editor/tests.js
@@ -23,113 +23,113 @@ describe('ChartTypeEditor', function() {
 
 
 describe('FacetsEditor', function() {
-  "use strict";
+    "use strict";
 
-  beforeEach(function() {
-      this.sandbox = sinon.sandbox.create();
-      this.sandbox.useFakeServer();
-      this.box = $('<div>').css('display', 'none').appendTo($('body'));
-  });
-
-  afterEach(function () {
-      this.sandbox.restore();
-      this.box.remove();
-  });
-
-  describe('facet list', function() {
-
-    it('should prefill dimensions', function() {
-        var model = new Backbone.Model();
-        var view = new App.FacetsEditor({model: model});
-        App.respond_json(this.sandbox.server.requests[0], [
-            {type_label: 'measure', notation: 'ignore me'},
-            {type_label: 'attribute', notation: 'ignore me too'},
-            {type_label: 'group dimension', notation: 'indicator-group'},
-            {type_label: 'dimension', notation: 'indicator'},
-            {type_label: 'group dimension', notation: 'breakdown-group'},
-            {type_label: 'dimension', notation: 'breakdown'},
-            {type_label: 'dimension', notation: 'unit-measure'},
-            {type_label: 'dimension', notation: 'ref-area'},
-            {type_label: 'dimension', notation: 'time-period'}
-        ]);
-
-        var facets = model.get('facets');
-        expect(facets[0]['dimension']).to.equal('indicator-group');
-        expect(facets[1]['dimension']).to.equal('indicator');
-        expect(facets[2]['dimension']).to.equal('breakdown-group');
-        expect(facets[3]['dimension']).to.equal('breakdown');
-        expect(facets[4]['dimension']).to.equal('unit-measure');
-        expect(facets[5]['dimension']).to.equal('ref-area');
-        expect(facets[6]['dimension']).to.equal('time-period');
+    beforeEach(function() {
+        this.sandbox = sinon.sandbox.create();
+        this.sandbox.useFakeServer();
+        this.box = $('<div>').css('display', 'none').appendTo($('body'));
     });
 
-    it('should save filter name same as dimension', function() {
-        var model = new Backbone.Model();
-        var view = new App.FacetsEditor({model: model, el: this.box});
-        App.respond_json(this.sandbox.server.requests[0], [
-            {type_label: 'dimension', notation: 'time-period'}
-        ]);
-        expect(model.get('facets')[0]['name']).to.equal('time-period');
-        expect(model.get('facets')[0]['dimension']).to.equal('time-period');
+    afterEach(function () {
+        this.sandbox.restore();
+        this.box.remove();
     });
 
-    it('should save filter type', function() {
-        var model = new Backbone.Model();
-        var view = new App.FacetsEditor({model: model, el: this.box});
-        App.respond_json(this.sandbox.server.requests[0], [
-            {type_label: 'dimension', notation: 'time-period'}
-        ]);
-        view.$el.find('select[name="type"]').val('data-column').change();
-        expect(model.get('facets')[0]['type']).to.equal('data-column');
-    });
+    describe('facet list', function() {
 
-    it('should create missing facets when loading config', function() {
-        var model = new Backbone.Model({
-            facets: [{name: 'time-period', type: 'data-column'}]
+        it('should prefill dimensions', function() {
+            var model = new Backbone.Model();
+            var view = new App.FacetsEditor({model: model});
+            App.respond_json(this.sandbox.server.requests[0], [
+                {type_label: 'measure', notation: 'ignore me'},
+                {type_label: 'attribute', notation: 'ignore me too'},
+                {type_label: 'group dimension', notation: 'indicator-group'},
+                {type_label: 'dimension', notation: 'indicator'},
+                {type_label: 'group dimension', notation: 'breakdown-group'},
+                {type_label: 'dimension', notation: 'breakdown'},
+                {type_label: 'dimension', notation: 'unit-measure'},
+                {type_label: 'dimension', notation: 'ref-area'},
+                {type_label: 'dimension', notation: 'time-period'}
+            ]);
+
+            var facets = model.get('facets');
+            expect(facets[0]['dimension']).to.equal('indicator-group');
+            expect(facets[1]['dimension']).to.equal('indicator');
+            expect(facets[2]['dimension']).to.equal('breakdown-group');
+            expect(facets[3]['dimension']).to.equal('breakdown');
+            expect(facets[4]['dimension']).to.equal('unit-measure');
+            expect(facets[5]['dimension']).to.equal('ref-area');
+            expect(facets[6]['dimension']).to.equal('time-period');
         });
-        var view = new App.FacetsEditor({model: model, el: this.box});
-        App.respond_json(this.sandbox.server.requests[0], [
-            {type_label: 'dimension', notation: 'indicator'},
-            {type_label: 'dimension', notation: 'time-period'}
-        ]);
-        expect(_(model.get('facets')).pluck('name')).to.deep.equal(
-            ['time-period', 'indicator']);
-    });
 
-  });
-
-  describe('facet type', function() {
-
-    it('should select existing filter type', function() {
-        var model = new Backbone.Model({
-            facets: [{name: 'time-period', type: 'data-column'}]
+        it('should save filter name same as dimension', function() {
+            var model = new Backbone.Model();
+            var view = new App.FacetsEditor({model: model, el: this.box});
+            App.respond_json(this.sandbox.server.requests[0], [
+                {type_label: 'dimension', notation: 'time-period'}
+            ]);
+            expect(model.get('facets')[0]['name']).to.equal('time-period');
+            expect(model.get('facets')[0]['dimension']).to.equal('time-period');
         });
-        var view = new App.FacetsEditor({model: model, el: this.box});
-        App.respond_json(this.sandbox.server.requests[0], [
-            {type_label: 'dimension', notation: 'time-period'}
-        ]);
-        var select = view.$el.find('select[name="type"]');
-        expect(select.val()).to.equal('data-column');
-    });
 
-    it('should reload from model on demand', function() {
-        var model = new Backbone.Model({
-            facets: [{name: 'time-period', type: 'data-column'}]
+        it('should save filter type', function() {
+            var model = new Backbone.Model();
+            var view = new App.FacetsEditor({model: model, el: this.box});
+            App.respond_json(this.sandbox.server.requests[0], [
+                {type_label: 'dimension', notation: 'time-period'}
+            ]);
+            view.$el.find('select[name="type"]').val('data-column').change();
+            expect(model.get('facets')[0]['type']).to.equal('data-column');
         });
-        var view = new App.FacetsEditor({model: model, el: this.box});
-        App.respond_json(this.sandbox.server.requests[0], [
-            {type_label: 'dimension', notation: 'time-period'}
-        ]);
-        var read_current_selection = function() {
-            return view.$el.find('select[name="type"]').val();
-        };
-        expect(read_current_selection()).to.equal('data-column');
-        model.set('facets', [{name: 'time-period', type: 'all-values'}]);
-        view.load_value();
-        expect(read_current_selection()).to.equal('all-values');
+
+        it('should create missing facets when loading config', function() {
+            var model = new Backbone.Model({
+                facets: [{name: 'time-period', type: 'data-column'}]
+            });
+            var view = new App.FacetsEditor({model: model, el: this.box});
+            App.respond_json(this.sandbox.server.requests[0], [
+                {type_label: 'dimension', notation: 'indicator'},
+                {type_label: 'dimension', notation: 'time-period'}
+            ]);
+            expect(_(model.get('facets')).pluck('name')).to.deep.equal(
+                ['time-period', 'indicator']);
+        });
+
     });
 
-  });
+    describe('facet type', function() {
+
+        it('should select existing filter type', function() {
+            var model = new Backbone.Model({
+                facets: [{name: 'time-period', type: 'data-column'}]
+            });
+            var view = new App.FacetsEditor({model: model, el: this.box});
+            App.respond_json(this.sandbox.server.requests[0], [
+                {type_label: 'dimension', notation: 'time-period'}
+            ]);
+            var select = view.$el.find('select[name="type"]');
+            expect(select.val()).to.equal('data-column');
+        });
+
+        it('should reload from model on demand', function() {
+            var model = new Backbone.Model({
+                facets: [{name: 'time-period', type: 'data-column'}]
+            });
+            var view = new App.FacetsEditor({model: model, el: this.box});
+            App.respond_json(this.sandbox.server.requests[0], [
+                {type_label: 'dimension', notation: 'time-period'}
+            ]);
+            var read_current_selection = function() {
+                return view.$el.find('select[name="type"]').val();
+            };
+            expect(read_current_selection()).to.equal('data-column');
+            model.set('facets', [{name: 'time-period', type: 'all-values'}]);
+            view.load_value();
+            expect(read_current_selection()).to.equal('all-values');
+        });
+
+    });
 
 });
 
