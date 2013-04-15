mateitrusca/scoreboard.visualization

commit a8f82cdaf6dd36bb8d3343a988d8b1f915ea6ebf

make series always have the same keys (fix this server-side)

scoreboard/visualization/jsapp/scenario/scenario.js
@@ -170,6 +170,7 @@ App.ScenarioChartView = Backbone.View.extend({
             var responses = _(arguments).toArray();
             if(requests.length < 2) { responses = [responses]; }
             chart_data['series'] = _(multiseries_values).map(function(value, n) {
+                //TODO resp should always have the same keys
                 var resp = responses[n];
                 var datapoints = resp[0]['datapoints'];
                 if(this.client_filter) {
@@ -180,7 +181,14 @@ App.ScenarioChartView = Backbone.View.extend({
                 }
                 return {
                     'label': chart_data['group_labels'][value],
-                    'data': datapoints
+                    'data': _(datapoints).map(function(item){
+                        var keys = _(item).keys().sort();
+                        var resp = _.object(
+                            ['code', 'label', 'value'],
+                            [item[keys[0]], item[keys[1]], item[keys[2]]]
+                        );
+                        return resp;
+                    })
                 };
             }, this);
             this.data = chart_data;

scoreboard/visualization/jsapp/chart/common.js
@@ -29,7 +29,6 @@ App.bar_colors = {
 
 
 App.format_series = function (data, sort, type){
-
     if (type=='xy'){
         var countrycolor = function(code) {
             if (_.isNull(App.COUNTRY_COLOR[code])) {
@@ -45,7 +44,7 @@ App.format_series = function (data, sort, type){
         };
 
         var series = _(data[0]['data']).map(function(datapoint) {
-            var code = datapoint['ref-area'];
+            var code = datapoint['code'];
             return {
                 'name': code,
                 'color': countrycolor(code),
@@ -71,8 +70,8 @@ App.format_series = function (data, sort, type){
         });
     }else{
         var extract_data = function(series_item){
-            return _.object([['name', series_item['ref-area-label']],
-                             ['code', series_item['ref-area']],
+            return _.object([['name', series_item['label']],
+                             ['code', series_item['code']],
                              ['y', series_item['value']]]);
         };
 
@@ -80,7 +79,7 @@ App.format_series = function (data, sort, type){
         var series = _.chain(data).map(function(item){
             var data = _(item['data']).map(extract_data);
             labels_collection = _.chain(item['data']).
-                                    pluck('ref-area-label').
+                                    pluck('label').
                                     union(labels_collection).
                                     value();
             return _.object(

scoreboard/visualization/jsapp/scenario/tests.js
@@ -9,8 +9,8 @@ describe('ChartSeriesPreparation', function() {
             {data: [],
              label:'2000'},
             {data: [{
-                        "ref-area": "AT",
-                        "ref-area-label": "Austria",
+                        "code": "AT",
+                        "label": "Austria",
                         "value": 0.4808
                     }],
              label:'2001'},
@@ -28,13 +28,13 @@ describe('ChartSeriesPreparation', function() {
         var series = [
             {data: [
                     {
-                        "ref-area": "BE",
-                        "ref-area-label": "Belgium",
+                        "code": "BE",
+                        "label": "Belgium",
                         "value": 0.3
                     },
                     {
-                        "ref-area": "BG",
-                        "ref-area-label": "Bulgaria",
+                        "code": "BG",
+                        "label": "Bulgaria",
                         "value": 0.4
                     },
 
@@ -42,13 +42,13 @@ describe('ChartSeriesPreparation', function() {
              label:'2000'},
             {data: [
                     {
-                        "ref-area": "BE",
-                        "ref-area-label": "Belgium",
+                        "code": "BE",
+                        "label": "Belgium",
                         "value": 0.4808
                     },
                     {
-                        "ref-area": "AT",
-                        "ref-area-label": "Austria",
+                        "code": "AT",
+                        "label": "Austria",
                         "value": 0.4808
                     },
 
@@ -66,8 +66,8 @@ describe('ChartSeriesPreparation', function() {
             {data: [],
              label:'2001'},
             {data: [{
-                        "ref-area": "AT",
-                        "ref-area-label": "Austria",
+                        "code": "AT",
+                        "label": "Austria",
                         "value": 0.4808
                     }],
              label:'2000'},
@@ -86,8 +86,8 @@ describe('ChartSeriesPreparation', function() {
             {data: [],
              label:'2003'},
             {data: [{
-                        "ref-area": "AT",
-                        "ref-area-label": "Austria",
+                        "code": "AT",
+                        "label": "Austria",
                         "value": 0.4808
                     }],
              label:'2001'},
@@ -393,8 +393,8 @@ describe('ScenarioChartView', function() {
     it('should render chart with the data received', function() {
         setup_scenario.apply(this);
         var server = this.sandbox.server;
-        var ajax_data = [{'ref-area': "Austria", 'value': 0.18},
-                         {'ref-area': "Belgium", 'value': 0.14}];
+        var ajax_data = [{'ref-area': 'AU','ref-area-label': "Austria", 'value': 0.18},
+                         {'ref-area': 'BE','ref-area-label': "Belgium", 'value': 0.14}];
 
         App.respond_json(server.requests[0], {'datapoints': ajax_data});
         App.respond_json(server.requests[1],
@@ -402,7 +402,9 @@ describe('ScenarioChartView', function() {
         expect(this.scenario_chart.calledOnce).to.equal(true);
         var call_args = this.scenario_chart.getCall(0).args;
         expect(call_args[0]).to.equal(this.chart.el);
-        expect(call_args[1]['series'][0]['data']).to.deep.equal(ajax_data);
+        var expected_data = [{'code': 'AU', 'label': "Austria", 'value': 0.18},
+                             {'code': 'BE', 'label': "Belgium", 'value': 0.14}];
+        expect(call_args[1]['series'][0]['data']).to.deep.equal(expected_data);
     });
 
     it('should make a single data query and then filter in JS', function() {
@@ -429,9 +431,9 @@ describe('ScenarioChartView', function() {
         expect(App.testing.url_param(url0, 'columns')).to.equal('dim3,dim4');
 
         App.respond_json(requests[0], {datapoints: [
-            {dim3: 'f3a', value: 13},
-            {dim3: 'f3b', value: 22},
-            {dim3: 'f3c', value: 10}
+            {dim3: 'f3a', lbl: 'a', value: 13},
+            {dim3: 'f3b', lbl: 'b', value: 22},
+            {dim3: 'f3c', lbl: 'c', value: 10}
         ]});
 
         var series = scenario_chart.getCall(0).args[1]['series'];

commit fb4e8e0629944b57e9079909f6a2b087b747c8e0

fix bug introduced in scatter scenario by a8f82cd

scoreboard/visualization/jsapp/scenario/scenario.js
@@ -188,11 +188,20 @@ App.ScenarioChartView = Backbone.View.extend({
                     'label': chart_data['group_labels'][value],
                     'data': _(datapoints).map(function(item){
                         var keys = _(item).keys().sort();
-                        var resp = _.object(
-                            ['code', 'label', 'value'],
-                            [item[keys[0]], item[keys[1]], item[keys[2]]]
-                        );
-                        return resp;
+                        var mapping = {
+                            'ref-area': 'code',
+                            'time-period': 'code',
+                            'ref-area-label': 'label',
+                            'time-period-label': 'label',
+                            'value': 'value'
+                        };
+                        var item_out = _.object();
+                        _(keys).each(function(key){
+                            item_out = _(item_out).extend(
+                                _.object([[mapping[key], item[key]]])
+                            );
+                        });
+                        return item_out;
                     })
                 };
             }, this);