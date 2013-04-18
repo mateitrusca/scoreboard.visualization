076353339a282ed84254a41efa2051dd512de676
David Batranu <dbatranu@gmail.com>
2013-04-18 15:27:19 +0300

    Event handler for datacube state change
    - unpublish all related visualizations when datacube is unpublished

---
+++ scoreboard/visualization/events/state.py
@@ -0,0 +1,17 @@
+from edw.datacube.interfaces import IDataCube
+from Products.CMFCore.utils import getToolByName
+
+
+def datacube_unpublished(obj):
+    wft =  getToolByName(obj, 'portal_workflow')
+    related = obj.getBRefs()
+    for item in related:
+        wft.doActionFor(item, 'retract')
+
+def datacube_handler(obj, evt):
+    if evt.action == 'retract':
+        datacube_unpublished(obj)
+
+def handler(obj, evt):
+    if IDataCube.providedBy(obj):
+        datacube_handler(obj, evt)
