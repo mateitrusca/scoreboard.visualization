4d69a0ca1e811ea29a31f0c1acd743f6eb797f13
Alex Morega <alex@grep.ro>
2013-04-30 18:32:01 +0300

    remove 'bars' chart type

--- scoreboard/visualization/jsapp/editor/chart_type.js
+++ scoreboard/visualization/jsapp/editor/chart_type.js
@@ -21,7 +21,6 @@ App.ChartTypeEditor = Backbone.View.extend({
         {label: "Line", value: 'lines'},
         {label: "Column", value: 'columns'},
         {label: "Column animation", value: 'evolution_columns'},
-        {label: "Bar", value: 'bars', disabled: true},
         {label: "Scatterplot", value: 'scatter'},
         {label: "Map", value: 'map'}
     ],
