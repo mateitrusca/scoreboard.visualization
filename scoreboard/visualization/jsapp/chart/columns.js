74457989068f9ca678498ce7a3febd51c9d4257c
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-05-16 17:38:29 +0300

    add plotlines(if specified in schema) to columns.js

--- scoreboard/visualization/jsapp/chart/columns.js
+++ scoreboard/visualization/jsapp/chart/columns.js
@@ -97,6 +97,10 @@ App.chart_library['columns'] = function(container, options) {
 
     var chart = new Highcharts.Chart(chartOptions);
 
+    if (options['plotlines']){
+        App.add_plotLines(chart, init_serie, options['plotlines']);
+    }
+
     if (options['animation']){
         if(!App.chart_controls){
             App.chart_controls = new App.GraphControlsView({
