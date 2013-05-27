5212359c2df098c9ad0ecbb1837cd9420d25ce0e
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-05-27 17:53:04 +0300

    trigger chart_ready from country_profile

--- scoreboard/visualization/jsapp/chart/country_profile.js
+++ scoreboard/visualization/jsapp/chart/country_profile.js
@@ -269,6 +269,8 @@ App.chart_library['country_profile'] = function(view, options) {
             x_formatter: x_formatter
         });
     }
+    view.trigger('chart_ready', series,
+        options['chart_type'] + '_' + options.subtype)
 };
 
 App.CountryProfileView = Backbone.View.extend({
