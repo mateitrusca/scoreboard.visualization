bb298a9b08861ccbc6c146e432b63aeac4224657
Andrei Melis <andrei.melis@eaudeweb.ro>
2013-05-27 18:42:45 +0300

    refactored cache warmer and added functions for country profile

--- scoreboard/visualization/jsapp/cache-warmup.js
+++ scoreboard/visualization/jsapp/cache-warmup.js
@@ -8,53 +8,72 @@ var urlParam = function(name){
     return results && results[1] || 0;
 }
 
-var depth = parseInt(urlParam('depth')) || 2;
+var depth = parseInt(urlParam('depth')) || 1;
 var DATA_REVISION;
 
 App.start = function() {
     var tokens = window.location.href.split("/");
     App.CUBE_URL = tokens.slice(0, tokens.length-1).join("/");
-    var message = "/revision";
-    log(message);
-    $.getJSON(App.CUBE_URL + "/revision", function(result){
+    App.getJSON('revision', null, function(result){
         DATA_REVISION = result;
-        App.dimension_options_indicator_groups();
-        // get indicators (simulate 'any' indicator group)
-        App.dimension_options_indicators();
-        // get all breakdowns - for dimension_value_metadata
-        App.dimension_options_breakdown();
-        log_done(message);
+        // get dimension_labels and dimension_value_metadata
+
+        App.dimension_value_method('dimension_labels', 'indicator-group');
+        App.dimension_value_method('dimension_value_metadata', 'indicator');
+        App.dimension_value_method('dimension_labels', 'indicator');
+        App.dimension_value_method('dimension_labels', 'breakdown-group');
+        App.dimension_value_method('dimension_value_metadata', 'breakdown');
+        App.dimension_value_method('dimension_labels', 'breakdown');
+        App.dimension_value_method('dimension_value_metadata', 'unit-measure');
+        App.dimension_value_method('dimension_labels', 'unit-measure');
+        App.dimension_value_method('dimension_labels', 'ref-area');
+        App.dimension_value_method('dimension_labels', 'time-period');
+
+        if ( depth > 1 ) {
+            App.dimension_options_indicator_groups();
+        }
     });
 };
 
-App.dimension_value_method = function(method, dimension, value) {
-    var args = {
-        'dimension': dimension,
-        'value': value,
-        'rev': DATA_REVISION
-    }; 
+
+App.getJSON = function(method, args, callback) {
     var message = "/" + method + "?" + serialize(args);
     log(message);
-    $.getJSON(App.CUBE_URL + "/" + method, args, function(data){
+    $.getJSON(App.CUBE_URL + "/" + method, args, function(data) {
         log_done(message);
+        if ( callback ) callback(data);
     });
 };
-
 
 App.dimension_options_indicator_groups = function() {
     var args = {
         'dimension': 'indicator-group',
         'rev': DATA_REVISION
-    }; 
-    var message = "/dimension_options?" + serialize(args);
-    log(message);
-    $.getJSON(App.CUBE_URL + "/dimension_options", args, function(data){
+    };
+    App.getJSON("dimension_options", args, function(data){
         _.each(data.options, function(option) {
             if ( depth > 1 ) {
                 App.dimension_options_indicators(option.notation);
             }
         });
-        log_done(message);
+    });
+    // get for country profile
+    App.dimension_options_indicator_groups_cp('bar');
+    App.dimension_options_indicator_groups_cp('table');
+};
+
+App.dimension_options_indicator_groups_cp = function(subtype) {
+    var args = {
+        'dimension': 'indicator-group',
+        'subtype': subtype,
+        'rev': DATA_REVISION
+    };
+    App.getJSON("dimension_options_cp", args, function(data){
+        _.each(data.options, function(option) {
+            if ( depth > 1 ) {
+                App.dimension_options_ref_area_cp(option.notation, subtype);
+            }
+        });
     });
 }
 
@@ -63,21 +82,13 @@ App.dimension_options_indicators = function(indicator_group) {
         'dimension': 'indicator',
         'indicator-group': indicator_group,
         'rev': DATA_REVISION
-    }; 
-    var message = "/dimension_options?" + serialize(args);
-    log(message);
-    $.getJSON(App.CUBE_URL + "/dimension_options", args, function(data){
+    };
+    App.getJSON("dimension_options", args, function(data){
         _.each(data.options, function(option) {
-            if (!indicator_group) {
-                // list with all indicators here
-                App.dimension_value_method('dimension_value_metadata', 'indicator', option.notation);
-                App.dimension_value_method('dimension_labels', 'indicator', option.notation);
-            }
             if ( depth > 2 ) {
                 App.dimension_options_breakdown_group(indicator_group, option.notation);
             }
         });
-        log_done(message);
     });
 }
 
@@ -87,16 +98,13 @@ App.dimension_options_breakdown_group = function(indicator_group, indicator) {
         'indicator-group': indicator_group,
         'indicator': indicator,
         'rev': DATA_REVISION
-    }; 
-    var message = "/dimension_options?" + serialize(args);
-    log(message);
-    $.getJSON(App.CUBE_URL + "/dimension_options", args, function(data){
+    };
+    App.getJSON("dimension_options", args, function(data){
         _.each(data.options, function(option) {
             if ( depth > 3 ) {
                 App.dimension_options_breakdown(indicator_group, indicator, option.notation);
             }
         });
-        log_done(message);
     });
     // and also simulate 'any' breakdown group
     App.dimension_options_breakdown(indicator_group, indicator);
@@ -109,22 +117,13 @@ App.dimension_options_breakdown = function(indicator_group, indicator, breakdown
         'indicator': indicator,
         'breakdown-group': breakdown_group,
         'rev': DATA_REVISION
-    }; 
-    var message = "/dimension_options?" + serialize(args);
-    log(message);
-    $.getJSON(App.CUBE_URL + "/dimension_options", args, function(data){
+    };
+    App.getJSON("dimension_options", args, function(data){
         _.each(data.options, function(option) {
-            if (!indicator_group && !indicator && !breakdown_group) {
-                // list with all breakdowns here
-                App.dimension_value_method('dimension_value_metadata', 'breakdown', option.notation);
-                App.dimension_value_method('dimension_labels', 'breakdown', option.notation);
-            }
-
             if ( depth > 4 ) {
                 App.dimension_options_unit_measure(indicator_group, indicator, breakdown_group, option.notation);
             }
         });
-        log_done(message);
     });
 }
 
@@ -136,19 +135,70 @@ App.dimension_options_unit_measure = function(indicator_group, indicator, breakd
         'breakdown-group': breakdown_group,
         'breakdown': breakdown,
         'rev': DATA_REVISION
-    }; 
-    var message = "/dimension_options?" + serialize(args);
-    log(message);
-    $.getJSON(App.CUBE_URL + "/dimension_options", args, function(data){
+    };
+    App.getJSON("dimension_options", args);
+}
+
+App.dimension_value_method = function(method, dimension) {
+    var args = {
+        'dimension': dimension,
+        'rev': DATA_REVISION
+    };
+    App.getJSON("dimension_options", args, function(data){
         _.each(data.options, function(option) {
-            if (!indicator_group && !indicator && !breakdown_group && !breakdown) {
-                // list with all units here
-                App.dimension_value_method('dimension_value_metadata', 'unit-measure', option.notation);
-                App.dimension_value_method('dimension_labels', 'unit-measure', option.notation);
+            var args2 = {
+                'dimension': dimension,
+                'value': option.notation,
+                'rev': DATA_REVISION
+            };
+            App.getJSON(method, args2);
+        });
+    });
+}
+
+App.dimension_options_ref_area_cp = function(indicator_group, subtype) {
+    var args = {
+        'dimension': 'ref-area',
+        'indicator-group': indicator_group,
+        'subtype': subtype,
+        'rev': DATA_REVISION
+    };
+    App.getJSON("dimension_options_cp", args, function(data){
+        _.each(data.options, function(option) {
+            if ( depth > 2 ) {
+                App.dimension_options_time_period_cp(option.notation, subtype);
             }
         });
-        log_done(message);
     });
+}
+
+App.dimension_options_time_period_cp = function(indicator_group, ref_area, subtype) {
+    var args = {
+        'dimension': 'time-period',
+        'indicator-group': indicator_group,
+        'ref-area': ref_area,
+        'subtype': subtype,
+        'rev': DATA_REVISION
+    };
+    App.getJSON("dimension_options_cp", args, function(data){
+        _.each(data.options, function(option) {
+            if ( depth > 3 ) {
+                App.dimension_options_indicator_cp(option.notation, subtype);
+            }
+        });
+    });
+}
+
+App.dimension_options_indicator_cp = function(indicator_group, ref_area, time_period, subtype) {
+    var args = {
+        'dimension': 'indicator',
+        'indicator-group': indicator_group,
+        'ref-area': ref_area,
+        'time-period': time_period,
+        'subtype': subtype,
+        'rev': DATA_REVISION
+    };
+    App.getJSON("dimension_options_cp", args);
 }
 
 var serialize = function(obj) {
