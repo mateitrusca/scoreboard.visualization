c7dd90cf843b56f568855cd25e8fa4b35da12df5
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-06-04 13:28:56 +0300

    increase contrast (close #139)

--- scoreboard/visualization/jsapp/common/common.js
+++ scoreboard/visualization/jsapp/common/common.js
@@ -50,7 +50,7 @@ App.COUNTRY_COLOR = _.object(_(country_data).pluck('code'),
 App.COUNTRY_NAME = _.object(_(country_data).pluck('code'),
                             _(country_data).pluck('label'));
 
-App.SERIES_COLOR = ['#377EB8', '#E41A1C', '#4DAF4A', '#984EA3', '#FF7F00', '#FFFF33', '#A65628', '#F781BF', '#0d233a'];
+App.SERIES_COLOR = ['#63b8ff', '#E41A1C', '#4DAF4A', '#984EA3', '#FF7F00', '#FFFF33', '#A65628', '#F781BF', '#0d233a'];
 
 App.index_by = function(list, prop) {
     return _.object(_(list).pluck(prop), list);
